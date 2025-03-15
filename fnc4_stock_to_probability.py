75b6a3a087d3d1eb2426c58fdbdb3d54cec84473
Sairaj182 <sairajraithatha.official@gmail.com>
2025-03-15 22:02:57 +0530

    completed function 4

--- fnc4_stock_to_probability.py
+++ fnc4_stock_to_probability.py
@@ -1,41 +1,5 @@
 # # function 4:- takes stock name, date list and returns stock and probability per stock name
 
-# import pandas as pd
-# from datetime import datetime
-# import os
-
-# base_dir = os.path.dirname(__file__)
-# # convert headline_sim list to a pandas dataframe
-
-# def stp(stock,datelist):
-#     df = pd.DataFrame(datelist)
-#     for i in range(len(df)):
-#         # date_u = df.iloc[i,0] #unformatted date
-#         # date_obj = datetime.strptime(str(date_u), "%Y%m%d")
-#         date_u = df.iloc[i, 0]
-#         if isinstance(date_u, str) and " " in date_u:  
-#             date_obj = datetime.fromisoformat(date_u.split()[0]) 
-#         else:
-#             date_obj = datetime.strptime(str(date_u), "%Y%m%d") 
-#         date = date_obj.strftime("%Y-%m-%d") #formatted date
-
-#         path = os.path.join(base_dir, "indicator_data_csvs", f"{stock}_indicator.csv")
-
-#         # Read CSV
-#         df_true = pd.read_csv(path)
-#         df = df_true.copy(deep=True)
-#         # print(df.head())
-#         container = []
-#         df["date_only"] = pd.to_datetime(df['Date']).dt.date #gives date of Date column excluding time as a date object
-        
-#         for j in df["date_only"]:
-#             if date == j:
-#                 container.append(df.iloc[j,2])
-#         print(container)
-        
-# datelist = [20230117,20220113,20240113]
-# stp("HDFCBANK", datelist)
-
 import pandas as pd
 from datetime import datetime
 import os
@@ -84,9 +48,26 @@ def stp(stock, datelist):
             results.append(None)  # Append None if date not found
 
     print("\nFinal Results:", results)  # Output the list of indicator values
+    sum = 0
+    nume = 0
+    for i in range(len(results)):
+        sum = sum+results[i]
+    if sum>0:
+        for j in range(len(results)):
+            if results[j]==1:
+                nume = nume+results[j]
+    elif sum<0:
+        for k in range(len(results)):
+            if results[k]==(-1):
+                nume = nume+results[k] 
+    else:
+        nume = 0
+    
+    probability = nume/(len(results)) 
+    return probability 
 
-# Example usage
-datelist = [20200313, 20220912, 20220912]
-stp("HDFCBANK", datelist)
+# # Example usage
+# datelist = [20200313, 20220912, 20220912]
+# print(stp("HDFCBANK", datelist))
 
 
